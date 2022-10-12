db8184393257b3250da0d35e361f19d46f1efab2
richard.chung <81546749+MadTurt1e@users.noreply.github.com>
2022-10-12 14:17:21 -0400

    modified:   FirstProgrammingAssignment/program1.cpp

--- FirstProgrammingAssignment/program1.cpp
+++ FirstProgrammingAssignment/program1.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main(){
-    try {
-  while (true) {cout << "."; cout.flush(); new char[1'000'000];}
-}
-catch (bad_alloc& b) {
-  cerr << "Memory exhausted: " << b.what() << '\n';
+
+
 }
 
+class foobar{
+  string 
 }
