f2be5ce87fd0dd12f75c51e00c4e1adecef43727
richard.chung <81546749+MadTurt1e@users.noreply.github.com>
2022-10-12 17:22:51 -0400

    modified:   .gitignore 	modified:   FirstProgrammingAssignment/program1.cpp 	deleted:    FirstProgrammingAssignment/program1.exe

--- FirstProgrammingAssignment/program1.cpp
+++ FirstProgrammingAssignment/program1.cpp
@@ -8,63 +8,75 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
-
-
-int powerLevel(){
-  return 0;
+//reads every line of a document and shoves the results into a foobar array
+string readInput(string inputName){
+  ifstream input(inputName);
+  string inString = "";
+  for (int i = 0; inString != "\n"; i++){
+    getline(input, inString);
+    cout << inString;
+  }
 }
-
 //Ordinary foobars who are standing in a line have a strength of equal to their position in the line. 
 class Foobar{
+  //The name of each foobar and its current position in a line should be stored in the base class as private data members. 
   private:
     string name; 
     int position;
   public: 
-    Foobar(string nam, int pos){
+  //The name should only be set once, in the base class's constructor.
+  //The constructor should initially set the position to 0, meaning that the foobar is not currently in a line.
+    Foobar(string nam){
       name = nam;
+      position = 0; 
+    }
+
+    //public member functions to set the position in a line
+    void setPosition(int pos){
       position = pos;
     }
 
-    //simply returns the name of the thing
+    //to get the strength 
+    int getStrength(){
+      return position;
+    }
+
+    //and to get the name
     string getName() const{
       return name;
     }
 
-    //returns the thing's position
-    int getPosition() const{
-      return position;
-    }
-
-    //powerlevel = position, returns position.
-    int getPowerLevel(){
-      return position;
-    }
+    protected: 
+      //a protected member function to get the position 
+      int getPosition() const{
+        return position;
+      }
 };
 
 //A foo's strength is equal to their position in the line multiplied by 3. 
 class Foo : public Foobar{
   public: 
     //position*3
-    int getPowerLevel(){
-      return position*3;
+    int getStrength(){
+      return getPosition()*3;
   }
 };
 
-//Some other foobars are bars. A bar's strength is equal to their position in the line plus 15.
+//A bar's strength is equal to their position in the line plus 15
 class Bar : public Foobar{
   public: 
     //position +15
-    int getPowerLevel(){
-      return position+15;
+    int getStrength(){
+      return getPosition()+15;
   }
 };
 
 //just kind of runs everything
 int main(){
-  Foobar a = Foobar("John", 2);
-  cout << a.getPowerLevel() << "\n" << "a";
+  readInput("sampleInput.txt");
   return 0;
 }
