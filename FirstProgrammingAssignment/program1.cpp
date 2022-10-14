efa778e6e449364994be92854a18c79d2a916709
richard.chung <81546749+MadTurt1e@users.noreply.github.com>
2022-10-13 21:52:15 -0400

    modified:   FirstProgrammingAssignment/program1.cpp

--- FirstProgrammingAssignment/program1.cpp
+++ FirstProgrammingAssignment/program1.cpp
@@ -12,15 +12,44 @@
 
 using namespace std;
 
-//reads every line of a document and shoves the results into a foobar array
+string readInput(string inputName);
+void* createFoobar(string name, string type);
+
+//Your program should loop through the input file only once, in a single pass adding information about each foobar into a vector of pointers to Foobar
 string readInput(string inputName){
   ifstream input(inputName);
-  string inString = "";
-  for (int i = 0; inString != "\n"; i++){
-    getline(input, inString);
-    cout << inString;
+  string type = "";
+  string name = "";
+  for (int i = 0; !input.eof(); i++){
+    getline(input, type, ' ');
+
+    //exit if we reach the end of the file. 
+    if(input.eof()){
+      break;
+    }
+
+    void* foobarObject; 
+    //protuces a foobar object
+    foobarObject = createFoobar(name, type);
   }
 }
+
+//creates Foobar objects
+void* createFoobar(string name, string type){
+  if (!type.compare("foobar")){
+    Foobar output = Foobar(name);
+    return &output; 
+  }
+  else if (!type.compare("foo")){
+    Foo output = Foo(name);
+    return &output; 
+  }
+  else if (!type.compare("bar")){
+    Bar output = Bar(name);
+    return &output;
+  }
+}
+
 //Ordinary foobars who are standing in a line have a strength of equal to their position in the line. 
 class Foobar{
   //The name of each foobar and its current position in a line should be stored in the base class as private data members. 
@@ -28,7 +57,6 @@ class Foobar{
     string name; 
     int position;
   public: 
-  //The name should only be set once, in the base class's constructor.
   //The constructor should initially set the position to 0, meaning that the foobar is not currently in a line.
     Foobar(string nam){
       name = nam;
@@ -60,6 +88,8 @@ class Foobar{
 //A foo's strength is equal to their position in the line multiplied by 3. 
 class Foo : public Foobar{
   public: 
+    //The name should only be set once, in the base class's constructor.
+    Foo(string name):Foobar(name){}
     //position*3
     int getStrength(){
       return getPosition()*3;
@@ -69,6 +99,7 @@ class Foo : public Foobar{
 //A bar's strength is equal to their position in the line plus 15
 class Bar : public Foobar{
   public: 
+    Bar(string name):Foobar(name){}
     //position +15
     int getStrength(){
       return getPosition()+15;
@@ -77,6 +108,8 @@ class Bar : public Foobar{
 
 //just kind of runs everything
 int main(){
+
+
   readInput("sampleInput.txt");
   return 0;
 }
