3a87a9cf84ea4f52934c45203a1eb04a1470f375
MadTurtle <81546749+MadTurt1e@users.noreply.github.com>
2022-11-14 21:35:07 -0500

    modified:   ListStackQueue/ListStackQueue.cpp

--- ListStackQueue/ListStackQueue.cpp
+++ ListStackQueue/ListStackQueue.cpp
@@ -1,179 +1,336 @@
 /*******************************************************************
  * Richard Chung                    ListStacKQueue.cpp
- * A program that manipulates stacks and queues, given input files. 
-*******************************************************************/
+ * A program that manipulates stacks and queues, given input files.
+ *******************************************************************/
 
 #include <iostream>
+#include <fstream>
 #include <list>
 
 using namespace std;
 
-
-
-
-//if the list is empty we want to throw an exception so nothing breaks. We can detect this exception so it should be fine. 
-class EmptyListException:public exception{
-	public:
-		const char* what()const throw(){
-			return "The list is empty!\n";
-		}
+////if the list is empty we want to throw an exception so nothing breaks. We can detect this exception so it should be fine.
+class EmptyListException : public exception
+{
+public:
+    const char *what() const throw()
+    {
+        return "The list is empty!\n";
+    }
 };
 
-//create an abstract base class called SimpleList that provides some aspects of singly linked list functionality
+// create an abstract base class called SimpleList that provides some aspects of singly linked list functionality
 template <typename T>
-class SimpleList{
-    private: 
-    
-    string name; 
+class SimpleList
+{
+private:
+    string name;
 
-    class Node{
-        private: 
-            //The base class should maintain pointers to the start and end of the singly linked list
-            T* currentValue = new T;
-            Node* nextValue = nullptr; 
-        public: 
-            //include a private nested class called Node, which contains one field for data and another to point to the next node
-            Node (T current, Node* next){
-                *currentValue = current;
-                nextValue = next;
-            }
-            Node (T* current, Node* next){
-                currentValue = current;
-                nextValue = next;
-            }
+    class Node
+    {
+    private:
+        // The base class should maintain pointers to the start and end of the singly linked list
+        T *currentValue = new T;
+        Node *nextValue = nullptr;
 
-            //changes currentValue to something else
-            void setCurrentValue(T* current){
-                currentValue = current;
-            }
-            //returns currentValue
-            T getCurrentValue(){
-                return *currentValue;
-            }
+    public:
+        // include a private nested class called Node, which contains one field for data and another to point to the next node
+        Node(T current, Node *next)
+        {
+            *currentValue = current;
+            nextValue = next;
+        }
+        Node(T *current, Node *next)
+        {
+            currentValue = current;
+            nextValue = next;
+        }
 
-            //changes the pointer to the next value
-            void setNextValue(Node* next){
-                nextValue = next;
-            }
-            //returns address of the nextValue
-            Node* getNextValue(){
-                return nextValue;
-            }
+        // changes currentValue to something else
+        void setCurrentValue(T *current)
+        {
+            currentValue = current;
+        }
+        // returns currentValue
+        T getCurrentValue()
+        {
+            return *currentValue;
+        }
+
+        // changes the pointer to the next value
+        void setNextValue(Node *next)
+        {
+            nextValue = next;
+        }
+        // returns address of the nextValue
+        Node *getNextValue()
+        {
+            return nextValue;
+        }
     };
 
-    Node* headerNode;
+    Node *headerNode;
 
-    //tail node makes changing around the final value more straightforward
-    Node* tailNode; 
-    int size; 
+    // tail node makes changing around the final value more straightforward
+    Node *tailNode;
+    int size;
 
-    public: 
+public:
+    // list constructor
+    SimpleList(string nam)
+    {
+        name = nam;
+        size = 0;
 
-        //list constructor
-        SimpleList(string nam){
-            name = nam; 
-            size = 0;
-            
-            tailNode = new Node(nullptr, nullptr);
-            headerNode = new Node(nullptr, tailNode);
+        tailNode = new Node(nullptr, nullptr);
+        headerNode = new Node(nullptr, tailNode);
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    // include two public pure virtual member functions for push and pop
+    virtual void push(T value) = 0;
+    virtual T pop() = 0;
+
+protected:
+    // provide the ability to insert a node at the start of the list
+    void insertAtStart(T value)
+    {
+        // Creates a new node, and shoves it in between header node and first node.
+        headerNode->setNextValue(new Node(value, headerNode->getNextValue()));
+
+        size++;
+    }
+
+    // to insert a node at the end of the list
+    void insertAtEnd(T value)
+    {
+        // new last node, we set the last node to cout << pSLi -> pop() << '\n';the value of the new one, and change the last node's memory address
+        Node *newLastNode = new Node(nullptr, nullptr);
+        *tailNode = *(new Node(value, newLastNode));
+        tailNode = newLastNode;
+        size++;
+    }
+    // to remove a node from the start of the listStack
+    T removeAtStart()
+    {
+        T output;
+        Node *pointerToStart;
+        // return null pointer if there is nothing
+        if (size != 0)
+        {
+            size--;
+            // point header to the second node, return value of first node, than remove first node.
+            pointerToStart = headerNode->getNextValue();
+            output = pointerToStart->getCurrentValue();
+            headerNode->setNextValue(pointerToStart->getNextValue());
+            return output;
         }
+        // empty
+        throw EmptyListException();
+    }
+};
 
-        string getName(){
-            return name; 
-        }
+template <typename T>
+class Stack : public SimpleList<T>
+{
+public:
+    Stack(string name) : SimpleList<T>(name)
+    {
+    }
+    void push(T value)
+    {
+        this->insertAtStart(value);
+    }
+    T pop()
+    {
+        return this->removeAtStart();
+    }
+};
 
-        //include two public pure virtual member functions for push and pop
-        virtual void push(T value) = 0;
-        virtual T pop() = 0;
+template <typename T>
+class Queue : public SimpleList<T>
+{
+public:
+    Queue(string name) : SimpleList<T>(name)
+    {
+    }
 
-    protected: 
-        //provide the ability to insert a node at the start of the list
-        void insertAtStart(T value){
-            //Creates a new node, and shoves it in between header node and first node. 
-            headerNode -> setNextValue(new Node(value, headerNode -> getNextValue()));
+    void push(T value)
+    {
+        this->insertAtEnd(value);
+    }
+    T pop()
+    {
+        return this->removeAtStart();
+    }
+};
 
-            size++;
-        }
+// function prototypes
+void readInput(string inputName, string outputName);
+template <typename T>
+SimpleList<T> *findList(list<SimpleList<T> *> searchThru, string name);
+template <typename T>
+void addToList(list<SimpleList<T> *> inputList, string name, string entry);
 
-        //to insert a node at the end of the list
-        void insertAtEnd(T value){
-            //new last node, we set the last node to cout << pSLi -> pop() << '\n';the value of the new one, and change the last node's memory address
-            Node* newLastNode = new Node(nullptr, nullptr);
-            *tailNode = *(new Node(value, newLastNode));
-            tailNode = newLastNode; 
-            size++;
-        }
-        //to remove a node from the start of the listStack
-        T removeAtStart(){
-            T output;
-            Node* pointerToStart;
-            //return null pointer if there is nothing
-            if (size != 0){
-                size--;
-                //point header to the second node, return value of first node, than remove first node. 
-                pointerToStart = headerNode -> getNextValue();
-                output = pointerToStart -> getCurrentValue();
-                headerNode -> setNextValue(pointerToStart -> getNextValue());
-                return output;
+// IN: filename
+// OUT: output file
+void readInput(string inputName, string outputName)
+{
+    list<SimpleList<int> *> listSLi;    // all integer stacks and queues
+    list<SimpleList<double> *> listSLd; // all double stacks and queues
+    list<SimpleList<string> *> listSLs; // all string stacks and queues
+
+    ifstream input(inputName);
+    ofstream output(outputName);
+
+    string action = "";
+    string name = "";
+    string entry = "";
+
+    // for keeping track of the type
+    char simpListType;
+
+    for (int i = 0; !input.eof(); i++)
+    {
+        output << "PROCESSING COMMAND: ";
+        getline(input, action, ' ');
+
+        // pop: only takes two entrees: pop, than name of stack / queue
+        if (action.compare("pop") == 0)
+        {
+            getline(input, name, '\n');
+
+            output << action << " " << name << '\n';
+
+            try
+            {
+                // do something different for each different data type
+                simpListType = name[0];
+
+                if (simpListType = 'i')
+                {
+                    output << findList(listSLi, name)->pop();
+                }
+                if (simpListType = 'd')
+                {
+                    output << findList(listSLd, name)->pop();
+                }
+                if (simpListType = 's')
+                {
+                    output << findList(listSLs, name)->pop();
+                }
             }
-            //empty
-            throw EmptyListException();
+            catch (EmptyListException)
+            {
+                output << "ERROR: This list is empty!" << '\n';
+            }
+            continue;
         }
-};
+        else
+        {
 
-template <typename T>
-class Stack : public SimpleList<T>{
-    public:
-        Stack(string name) : SimpleList<T>(name){
+            getline(input, name, ' ');
+            simpListType = name[0];
 
+            getline(input, entry, '\n');
         }
-        void push(T value){
-            this -> insertAtStart(value);
-        }
-        T pop(){
-            return this -> removeAtStart();
-        }
-}; 
 
-template <typename T>
-class Queue : public SimpleList<T>{
-    public: 
-        Queue(string name) : SimpleList<T>(name){
+        // create: takes three entries: create, name, stack/queue
+        if (action.compare("create") == 0)
+        {
+            // steps: we check to make sure the name isn't already there, then we add the thing in.
+            if (simpListType = 'i')
+            {
+                if (findList(listSLi, name) != nullptr)
+                {
+                    addToList(listSLi, name, entry);
+                }
+                else
+                {
+                    output << "ERROR: This name already exists!" << '\n';
+                    continue;
+                }
+            }
+            if (simpListType = 'd')
+            {
+                if (findList(listSLd, name) != nullptr)
+                {
+                    addToList(listSLd, name, entry);
+                }
+                else
+                {
+                    output << "ERROR: This name already exists!" << '\n';
+                    continue;
+                }
+            }
+            if (simpListType = 's')
+            {
+                if (findList(listSLs, name) != nullptr)
+                {
+                    addToList(listSLs, name, entry);
+                }
+                else
+                {
+                    output << "ERROR: This name already exists!" << '\n';
+                    continue;
+                }
+            }
+        }
+        // push: takes three entries: push, name, data value
+        if (action.compare("push") == 0)
+        {
             
         }
-
-        void push(T value){
-            this -> insertAtEnd(value);
+        //  exit if we reach the end of the file.
+        if (input.eof())
+        {
+            break;
         }
-        T pop(){
-            return this -> removeAtStart();
-        }
-};
+    }
 
-//returns a pointer to the correct simpleList with name
+    input.close();
+    output.close();
+    return;
+}
+
+// returns a pointer to the correct simpleList with name
 template <typename T>
-SimpleList<T>* findList(list<SimpleList<T>*> searchThru, string name){
-    for (int i = 0; i < searchThru.size(); i++){
-        if (name.compare(searchThru.getName()) == 0){
-            return //stuff
+SimpleList<T> *findList(list<SimpleList<T> *> searchThru, string name)
+{
+    for (auto pos = searchThru.begin(); pos != searchThru.end(); pos++)
+    {
+        if (name.compare((*pos)->getName()) == 0)
+        {
+            return *pos;
         }
     }
     return nullptr;
 }
 
-int main(){
-    list<SimpleList<int> *> listSLi; // all integer stacks and queues
-    list<SimpleList<double> *> listSLd; // all double stacks and queues
-    list<SimpleList<string> *> listSLs; // all string stacks and queues
+// creates a new simple list given a name and type of list, and shoves it into a given list.
+template <typename T>
+void addToList(list<SimpleList<T> *> inputList, string name, string entry)
+{
+    SimpleList<T> *newSimpleList;
 
-    SimpleList<string>* pSLi;
-    pSLi = new Queue<string>("sample1");
-    pSLi -> push("a");
-    pSLi -> push("b");
-    cout << pSLi -> pop() << '\n';
-    pSLi -> push("c");
-    cout << pSLi -> pop() << '\n';
-    cout << pSLi -> pop() << '\n';
-    cout << pSLi -> pop() << '\n';
+    if (entry.compare("stack") == 0)
+    {
+        newSimpleList = new Stack<T>(name);
+    }
+    else
+    {
+        newSimpleList = new Queue<T>(name);
+    }
+    inputList.push_front(newSimpleList);
+
+    return;
+}
+
+int main()
+{
     return 0;
 }
