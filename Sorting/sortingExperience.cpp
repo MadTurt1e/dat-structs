6b15ddf15a72ef0cbab78d0bb9fb654877f15bf5
richard.chung <81546749+MadTurt1e@users.noreply.github.com>
2022-12-01 20:49:39 -0500

    new file:   Sorting/firstNames.txt 	new file:   Sorting/lastNames.txt 	modified:   Sorting/sortingExperience.cpp

--- Sorting/sortingExperience.cpp
+++ Sorting/sortingExperience.cpp
@@ -83,7 +83,8 @@ void sortDataList(list<Data *> &);
 int main() {
   string filename;
   cout << "Enter name of input file: ";
-  cin >> filename;
+  filename = "input2.txt";
+  // cin >> filename;
   list<Data *> theList;
   loadDataList(theList, filename);
 
@@ -98,7 +99,8 @@ int main() {
   cout << "Sort finished. CPU time was " << timeDiff << " seconds.\n";
 
   cout << "Enter name of output file: ";
-  cin >> filename;
+  filename = "output";
+  // cin >> filename;
   writeDataList(theList, filename);
 
   return 0;
@@ -118,16 +120,20 @@ int main() {
 *********************************************************/
 
 #include <queue>
-//vector of 90 as in there are 90 ascii values from NUL to Z. 
-vector<queue<Data*>> bucketOfBuckets(90);
+//bucket that can be reused for a bunch of stuff
+vector<queue<Data*>> bucketOfBuckets(10000);
+
+//literally every single first/last name for radix sorting. its preordered so its easier. 
+vector<string> firstNamesSorted = {"AALIYAH", "AARON", "ABEL", "ABIGAIL", "ABRAHAM", "ADALINE", "ADALYN", "ADALYNN", "ADAM", "ADDISON", "ADELINE", "ADELYN", "ADRIAN", "ADRIANA", "AIDAN", "AIDEN", "ALAINA", "ALAN", "ALANA", "ALAYNA", "ALEJANDRO", "ALEX", "ALEXA", "ALEXANDER", "ALEXANDRA", "ALEXIS", "ALICE", "ALINA", "ALIVIA", "ALIYAH", "ALLISON", "ALYSSA", "AMARA", "AMAYA", "AMELIA", "AMIR", "AMY", "ANA", "ANASTASIA", "ANDREA", "ANDRES", "ANDREW", "ANGEL", "ANGELA", "ANGELINA", "ANNA", "ANNABELLE", "ANTHONY", "ANTONIO", "ARABELLA", "ARIA", "ARIANA", "ARIANNA", "ARIEL", "ARTHUR", "ARYA", "ASHER", "ASHLEY", "ASHTON", "ATHENA", "AUBREE", "AUBREY", "AUDREY", "AUGUST", "AURORA", "AUSTIN", "AUTUMN", "AVA", "AVERY", "AVERY", "AXEL", "AYDEN", "AYLA", "BAILEY", "BARRETT", "BEAU", "BECKETT", "BELLA", "BENJAMIN", "BENNETT", "BENTLEY", "BLAKE", "BRADLEY", "BRADY", "BRANDON", "BRANTLEY", "BRAXTON", "BRAYDEN", "BRIAN", "BRIANNA", "BRIELLE", "BRODY", "BROOKE", "BROOKLYN", "BROOKLYNN", "BROOKS", "BRYAN", "BRYCE", "BRYNLEE", "BRYSON", "CADEN", "CALEB", "CALLIE", "CALVIN", "CAMDEN", "CAMERON", "CAMILA", "CARLOS", "CAROLINE", "CARSON", "CARTER", "CATHERINE", "CAYDEN", "CECILIA", "CHARLES", "CHARLIE", "CHARLIE", "CHARLOTTE", "CHASE", "CHLOE", "CHRISTIAN", "CHRISTOPHER", "CLAIRE", "CLARA", "CLAYTON", "COLE", "COLIN", "COLTON", "CONNOR", "COOPER", "CORA", "DAISY", "DAKOTA", "DALEYZA", "DAMIAN", "DANIEL", "DANIELA", "DAVID", "DAWSON", "DEAN", "DECLAN", "DELANEY", "DELILAH", "DEREK", "DESTINY", "DIANA", "DIEGO", "DOMINIC", "DYLAN", "EASTON", "EDEN", "EDWARD", "ELEANOR", "ELENA", "ELI", "ELIANA", "ELIAS", "ELIJAH", "ELISE", "ELIZA", "ELIZABETH", "ELLA", "ELLIANA", "ELLIE", "ELLIOT", "ELLIOTT", "ELOISE", "EMERSON", "EMERSYN", "EMERY", "EMILIA", "EMILIANO", "EMILY", "EMMA", "EMMANUEL", "EMMETT", "ERIC", "ESTHER", "ETHAN", "EVA", "EVAN", "EVELYN", "EVERETT", "EVERLY", "EZEKIEL", "EZRA", "FAITH", "FELIX", "FINLEY", "FINN", "FIONA", "GABRIEL", "GABRIELLA", "GAEL", "GAVIN", "GENESIS", "GENEVIEVE", "GEORGE", "GEORGIA", "GIANNA", "GIOVANNI", "GRACE", "GRACIE", "GRAHAM", "GRANT", "GRAYSON", "GREYSON", "GRIFFIN", "HADLEY", "HAILEY", "HANNAH", "HARLEY", "HARMONY", "HARPER", "HARRISON", "HAYDEN", "HAYDEN", "HAZEL", "HENRY", "HOLDEN", "HUDSON", "HUNTER", "IAN", "IRIS", "ISAAC", "ISABEL", "ISABELLA", "ISABELLE", "ISAIAH", "ISLA", "ISRAEL", "IVAN", "IVY", "JACE", "JACK", "JACKSON", "JACOB", "JADE", "JADEN", "JAKE", "JAMES", "JAMESON", "JASMINE", "JASON", "JASPER", "JAVIER", "JAX", "JAXON", "JAXSON", "JAYCE", "JAYDEN", "JAYLA", "JEREMIAH", "JEREMY", "JESSE", "JESSICA", "JESUS", "JOANNA", "JOCELYN", "JOEL", "JOHN", "JONAH", "JONATHAN", "JORDAN", "JORDYN", "JORGE", "JOSE", "JOSEPH", "JOSEPHINE", "JOSHUA", "JOSIAH", "JOSIE", "JOSUE", "JUAN", "JUDAH", "JUDE", "JULIA", "JULIAN", "JULIANA", "JULIANNA", "JULIET", "JULIETTE", "JUNE", "JUSTIN", "KADEN", "KAI", "KAIDEN", "KALEB", "KARTER", "KATHERINE", "KAYDEN", "KAYLA", "KAYLEE", "KENDALL", "KENNEDY", "KENNETH", "KEVIN", "KHLOE", "KILLIAN", "KIMBERLY", "KING", "KINGSTON", "KINSLEY", "KNOX", "KYLE", "KYLIE", "KYRIE", "LAILA", "LANDON", "LAUREN", "LAYLA", "LEAH", "LEILA", "LEILANI", "LEO", "LEON", "LEONARDO", "LEVI", "LIAM", "LILA", "LILIANA", "LILLIAN", "LILLY", "LILY", "LINCOLN", "LOGAN", "LOLA", "LONDON", "LONDYN", "LORENZO", "LUCA", "LUCAS", "LUCIA", "LUCY", "LUIS", "LUKAS", "LUKE", "LUNA", "LYDIA", "LYLA", "MACKENZIE", "MADDOX", "MADELINE", "MADELYN", "MADISON", "MAGGIE", "MAKAYLA", "MALACHI", "MALIA", "MARCUS", "MARGARET", "MARIA", "MARIAH", "MARK", "MARLEY", "MARY", "MASON", "MATEO", "MATIAS", "MATTEO", "MATTHEW", "MAVERICK", "MAX", "MAXIMUS", "MAXWELL", "MAYA", "MCKENZIE", "MELANIE", "MELODY", "MESSIAH", "MIA", "MICAH", "MICHAEL", "MICHELLE", "MIGUEL", "MILA", "MILES", "MILO", "MOLLY", "MORGAN", "MYA", "MYLES", "NAOMI", "NATALIA", "NATALIE", "NATHAN", "NATHANIEL", "NEVAEH", "NICHOLAS", "NICOLAS", "NICOLE", "NOAH", "NOELLE", "NOLAN", "NORA", "NORAH", "NOVA", "OLIVER", "OLIVIA", "OMAR", "OSCAR", "OWEN", "PAIGE", "PAISLEY", "PARKER", "PARKER", "PATRICK", "PAUL", "PAXTON", "PAYTON", "PENELOPE", "PETER", "PEYTON", "PIPER", "PRESLEY", "PRESTON", "QUINN", "RACHEL", "RAELYNN", "REAGAN", "REBECCA", "REESE", "REMI", "REMINGTON", "RHETT", "RICHARD", "RILEY", "RIVER", "ROBERT", "ROMAN", "ROSALIE", "ROSE", "ROWAN", "ROWAN", "RUBY", "RYAN", "RYDER", "RYKER", "RYLEE", "RYLEIGH", "SADIE", "SAMANTHA", "SAMUEL", "SANTIAGO", "SARA", "SARAH", "SAVANNAH", "SAWYER", "SAWYER", "SCARLETT", "SEBASTIAN", "SELENA", "SERENITY", "SIENNA", "SILAS", "SKYLAR", "SLOANE", "SOFIA", "SOPHIA", "SOPHIE", "STELLA", "STEVEN", "SUMMER", "SYDNEY", "TAYLOR", "TEAGAN", "TESSA", "THEODORE", "THIAGO", "THOMAS", "TIMOTHY", "TRINITY", "TRISTAN", "TUCKER", "TYLER", "VALENTINA", "VALERIA", "VALERIE", "VANESSA", "VICTOR", "VICTORIA", "VINCENT", "VIOLET", "VIVIAN", "WAYLON", "WESLEY", "WESTON", "WILLIAM", "WILLOW", "WYATT", "XANDER", "XAVIER", "XIMENA", "ZACHARY", "ZANDER", "ZANE", "ZAYDEN", "ZION", "ZOE", "ZOEY"};
+vector<string> lastNamesSorted = {"ACOSTA", "ADAMS", "ADKINS", "AGUILAR", "AGUIRRE", "ALEXANDER", "ALLEN", "ALVARADO", "ALVAREZ", "ANDERSON", "ANDREWS", "ARMSTRONG", "ARNOLD", "AUSTIN", "AVILA", "AYALA", "BAILEY", "BAKER", "BALDWIN", "BANKS", "BARBER", "BARKER", "BARNES", "BARNETT", "BARRETT", "BARTON", "BATES", "BECK", "BECKER", "BELL", "BENNETT", "BENSON", "BERRY", "BISHOP", "BLACK", "BLAIR", "BLAKE", "BOWEN", "BOWMAN", "BOYD", "BRADLEY", "BRADY", "BREWER", "BROOKS", "BROWN", "BRYANT", "BURGESS", "BURKE", "BURNS", "BURTON", "BUSH", "BUTLER", "BYRD", "CABRERA", "CALDERON", "CALDWELL", "CAMACHO", "CAMPBELL", "CAMPOS", "CANNON", "CARDENAS", "CARLSON", "CARPENTER", "CARR", "CARRILLO", "CARROLL", "CARTER", "CASTANEDA", "CASTILLO", "CASTRO", "CERVANTES", "CHAMBERS", "CHAN", "CHANDLER", "CHANG", "CHAPMAN", "CHAVEZ", "CHEN", "CHRISTENSEN", "CLARK", "CLARKE", "COHEN", "COLE", "COLEMAN", "COLLINS", "COLON", "CONTRERAS", "COOK", "COOPER", "CORTEZ", "COX", "CRAIG", "CRAWFORD", "CROSS", "CRUZ", "CUMMINGS", "CUNNINGHAM", "CURRY", "CURTIS", "DANIEL", "DANIELS", "DAVIDSON", "DAVIS", "DAWSON", "DAY", "DEAN", "DELACRUZ", "DELEON", "DELGADO", "DENNIS", "DIAZ", "DIXON", "DOMINGUEZ", "DOUGLAS", "DOYLE", "DUNCAN", "DUNN", "DURAN", "EDWARDS", "ELLIOTT", "ELLIS", "ERICKSON", "ESPINOZA", "ESTRADA", "EVANS", "FARMER", "FERGUSON", "FERNANDEZ", "FIELDS", "FIGUEROA", "FISCHER", "FISHER", "FITZGERALD", "FLEMING", "FLETCHER", "FLORES", "FORD", "FOSTER", "FOWLER", "FOX", "FRANCIS", "FRANCO", "FRANK", "FRANKLIN", "FRAZIER", "FREEMAN", "FUENTES", "FULLER", "GALLAGHER", "GALLEGOS", "GARCIA", "GARDNER", "GARNER", "GARRETT", "GARZA", "GEORGE", "GIBSON", "GILBERT", "GILL", "GOMEZ", "GONZALES", "GONZALEZ", "GOODMAN", "GOODWIN", "GORDON", "GRAHAM", "GRANT", "GRAVES", "GRAY", "GREEN", "GREENE", "GREGORY", "GRIFFIN", "GRIFFITH", "GROSS", "GUERRA", "GUERRERO", "GUTIERREZ", "GUZMAN", "HAIL", "HALE", "HALL", "HAMILTON", "HAMMOND", "HAMPTON", "HANSEN", "HANSON", "HARDY", "HARMON", "HARPER", "HARRINGTON", "HARRIS", "HARRISON", "HART", "HARVEY", "HAWKINS", "HAYES", "HAYNES", "HENDERSON", "HENRY", "HERNANDEZ", "HERRERA", "HICKS", "HIGGINS", "HILL", "HINES", "HODGES", "HOFFMAN", "HOLLAND", "HOLMES", "HOLT", "HOPKINS", "HORTON", "HOWARD", "HOWELL", "HUANG", "HUBBARD", "HUDSON", "HUGHES", "HUNT", "HUNTER", "INGRAM", "JACKSON", "JACOBS", "JAMES", "JENKINS", "JENNINGS", "JENSEN", "JIMENEZ", "JOHNSON", "JOHNSTON", "JONES", "JORDAN", "JOSEPH", "JUAREZ", "KELLER", "KELLEY", "KELLY", "KENNEDY", "KHAN", "KIM", "KING", "KLEIN", "KNIGHT", "LAMBERT", "LANE", "LARA", "LARSON", "LAWRENCE", "LAWSON", "LE", "LEE", "LEON", "LEONARD", "LEWIS", "LI", "LIN", "LITTLE", "LIU", "LOGAN", "LONG", "LOPEZ", "LOVE", "LOWE", "LUCAS", "LUNA", "LYNCH", "LYONS", "MACK", "MALDONADO", "MALONE", "MANN", "MANNING", "MARQUEZ", "MARSHALL", "MARTIN", "MARTINEZ", "MASON", "MATTHEWS", "MAXWELL", "MAY", "MCCARTHY", "MCCOY", "MCDANIEL", "MCDONALD", "MCGEE", "MCKINNEY", "MCLAUGHLIN", "MEDINA", "MEJIA", "MENDEZ", "MENDOZA", "MEYER", "MILES", "MILLER", "MILLS", "MIRANDA", "MITCHELL", "MOLINA", "MONTGOMERY", "MONTOYA", "MOORE", "MORALES", "MORAN", "MORENO", "MORGAN", "MORRIS", "MORRISON", "MOSS", "MULLINS", "MUNOZ", "MURPHY", "MURRAY", "MYERS", "NAVARRO", "NEAL", "NELSON", "NEWMAN", "NEWTON", "NGUYEN", "NICHOLS", "NORMAN", "NORRIS", "NUNEZ", "OBRIEN", "OCHOA", "OCONNOR", "OLIVER", "OLSON", "ORTEGA", "ORTIZ", "OWENS", "PACHECO", "PADILLA", "PAGE", "PALMER", "PARK", "PARKER", "PARKS", "PARSONS", "PATEL", "PATTERSON", "PAUL", "PAYNE", "PEARSON", "PENA", "PEREZ", "PERKINS", "PERRY", "PERSON", "PETERS", "PETERSON", "PHAM", "PHILLIPS", "PIERCE", "PORTER", "POTTER", "POWELL", "POWERS", "PRICE", "QUINN", "RAMIREZ", "RAMOS", "RAMSEY", "RAY", "REED", "REESE", "REEVES", "REID", "REYES", "REYNOLDS", "RHODES", "RICE", "RICHARDS", "RICHARDSON", "RILEY", "RIOS", "RIVAS", "RIVERA", "ROBBINS", "ROBERTS", "ROBERTSON", "ROBINSON", "ROBLES", "RODGERS", "RODRIGUEZ", "ROGERS", "ROJAS", "ROMAN", "ROMERO", "ROSALES", "ROSE", "ROSS", "ROWE", "RUIZ", "RUSSELL", "RYAN", "SALAZAR", "SALINAS", "SANCHEZ", "SANDERS", "SANDOVAL", "SANTIAGO", "SANTOS", "SAUNDERS", "SCHMIDT", "SCHNEIDER", "SCHROEDER", "SCHULTZ", "SCHWARTZ", "SCOTT", "SERRANO", "SHARP", "SHAW", "SHELTON", "SHERMAN", "SILVA", "SIMMONS", "SIMON", "SIMPSON", "SIMS", "SINGH", "SMITH", "SNYDER", "SOLIS", "SOTO", "SPENCER", "STANLEY", "STEELE", "STEPHENS", "STEVENS", "STEVENSON", "STEWART", "STONE", "STRICKLAND", "SULLIVAN", "SUTTON", "SWANSON", "TATE", "TAYLOR", "TERRY", "THOMAS", "THOMPSON", "THORNTON", "TODD", "TORRES", "TOWNSEND", "TRAN", "TRUJILLO", "TUCKER", "TURNER", "VALDEZ", "VALENCIA", "VARGAS", "VASQUEZ", "VAUGHN", "VAZQUEZ", "VEGA", "VELASQUEZ", "WADE", "WAGNER", "WALKER", "WALLACE", "WALSH", "WALTERS", "WALTON", "WANG", "WARD", "WARNER", "WARREN", "WASHINGTON", "WATERS", "WATKINS", "WATSON", "WATTS", "WEAVER", "WEBB", "WEBER", "WEBSTER", "WELCH", "WELLS", "WEST", "WHEELER", "WHITE", "WILLIAMS", "WILLIAMSON", "WILLIS", "WILSON", "WISE", "WOLF", "WOLFE", "WONG", "WOOD", "WOODS", "WRIGHT", "WU", "YANG", "YOUNG", "ZHANG", "ZIMMERMAN"};
 
 
 bool myLessThanFull(Data* d1, Data* d2); 
 int convertSSN(string ssn);
-int listType(list<Data *> l);
-void radixSort(list<Data *> &);
-void radixSortSSN(list<Data*> &);
-void insertionSort(list<Data*> &, int, int);
+int listType(list<Data *>* l);
+void radixSort(list<Data *>*);
+void radixSortSSN(list<Data*>*);
+void insertionSort(list<Data*>*, int, int);
 
 //compares two Data. True if d1 less than d2, false otherwise. 
 bool myLessThanFull(Data* d1, Data* d2){
@@ -169,8 +175,8 @@ int convertSSN(string ssn){
 
 
 //determine what list type it is. There are 3 types: full RNG (type 1), only social security numbers unsorted (type 2), and everybody with the same name (type 3)
-int listType(list<Data *> l){
-  auto i = l.begin();
+int listType(list<Data *>* l){
+  auto i = l -> begin();
 
   string lName = (*i)->lastName;
   string fName = (*i)->firstName;
@@ -184,11 +190,7 @@ int listType(list<Data *> l){
 
   int numberOfRepititions = 10;
 
-  for (int number = 0; number < numberOfRepititions || i == l.end(); i++, number++){
-
-   
-
-    
+  for (int number = 0; number < numberOfRepititions || i == l->end(); advance(i, 100), number++){
     lNameComp = lName.compare((*i)->lastName);
 
     //if it is less than 0 we know first is less than second, so maybe type 2? Well, we know for sure it's not type 3. 
@@ -203,7 +205,7 @@ int listType(list<Data *> l){
       fNameComp = fName.compare((*i)->firstName);
 
       //We check if the first name is in order
-      if(fNameComp){
+      if(fNameComp < 0){
         fName = (*i)-> firstName;
         dataType = 2;
       }
@@ -224,16 +226,74 @@ int listType(list<Data *> l){
 }
 
 //raddix sort! calss the ssn sort to sort ssns, but does some other wacky stuff later
-void radixSort(list<Data*> &l){
+void radixSort(list<Data*>* l){
   //step 1: sort by SSNs first
   radixSortSSN(l);
 
-  //step 2: sort first names
+  int radixing = 0;
 
+  //step 2: sort first names
+  list<Data*>::iterator it;
+
+  for (it = l -> begin(); it != l -> end(); ++it){
+    for (radixing = 0; radixing < firstNamesSorted.size(); radixing++){
+      if ((*it) -> firstName == firstNamesSorted[radixing]){
+        break;
+      }
+    }//when we break out of the loop we will have the index to stick the name in
+    bucketOfBuckets[radixing].push(*it);
+  }//after we break out of this loop we will have all the first names sorted.
+
+  it = l->begin();
+  // stick it back into the list
+  for (int j = 0; j <= 500; j++)
+  {
+    // pop all the values until the thing is empty
+    while (!bucketOfBuckets[j].empty())
+    {
+      // kick it out of the bucket
+      (*it) = (bucketOfBuckets[j].front());
+      bucketOfBuckets[j].pop();
+      ++it;
+    }
+    if (it == l->end())
+    {
+      break;
+    }
+  }//first names are now sorted. 
+
+
+  //step 2: sort last names. 
+  for (it = l -> begin(); it != l -> end(); ++it){
+    for (radixing = 0; radixing < lastNamesSorted.size(); radixing++){
+      if ((*it) -> lastName == (lastNamesSorted[radixing])){
+        break;
+      }
+    }//when we break out of the loop we will have the index to stick the name in
+    bucketOfBuckets[radixing].push(*it);
+  }//after we break out of this loop we will have all the first names sorted.
+
+  it = l->begin();
+  // stick it back into the list
+  for (int j = 0; j <= 500; j++)
+  {
+    // pop all the values until the thing is empty
+    while (!bucketOfBuckets[j].empty())
+    {
+      // kick it out of the bucket
+      (*it) = (bucketOfBuckets[j].front());
+      bucketOfBuckets[j].pop();
+      ++it;
+    }
+    if (it == l->end())
+    {
+      break;
+    }
+  }
 }
 
 // uses raddix sort to sort ssns only
-void radixSortSSN(list<Data *> &l)
+void radixSortSSN(list<Data *>* l)
 {
   clock_t t1 = clock();
   // goes through all 11 SSN characters
@@ -244,12 +304,12 @@ void radixSortSSN(list<Data *> &l)
       continue;
     }
     // scan the entire list
-    for (auto i = l.begin(); i != l.end(); i++)
+    for (auto i = l -> begin(); i != l -> end(); ++i)
     {
       bucketOfBuckets[((*i)->ssn)[charNum]].push(*i); // stick the data pointer into the bucket
     }
 
-    list<Data*>::iterator it = l.begin();
+    list<Data*>::iterator it = l -> begin();
     // sticks all the values from the relevant superbucket buckets back into the original vector
     for (int j = 48; j <= 57; j++)
     {
@@ -258,9 +318,9 @@ void radixSortSSN(list<Data *> &l)
       {
         (*it) = (bucketOfBuckets[j].front());
         bucketOfBuckets[j].pop();
-        it++;
+        ++it;
       }
-      if (it == l.end()){
+      if (it == l->end()){
         break;
       }
     }
@@ -268,17 +328,17 @@ void radixSortSSN(list<Data *> &l)
 }
 
 //insertion sort! Runs for the small chunks found in type2 sorting where both first and last names are the same. 
-void insertionSort(list<Data*> &l, int start, int end){
+void insertionSort(list<Data*>* l, int start, int end){
 
 }
 
-void type1Sort(list<Data*> &l){
+void type1Sort(list<Data*>* l){
   radixSort(l);
 }
-void type2Sort(list<Data*> &l){
+void type2Sort(list<Data*>* l){
   
 }
-void type3Sort(list<Data*> &l){
+void type3Sort(list<Data*>* l){
   radixSortSSN(l);
 }
 
@@ -286,19 +346,20 @@ void type3Sort(list<Data*> &l){
 void sortDataList(list<Data *> &l)
 {
   // step 1: go through the first 5 or so data members to figure out which list classification it is.
-  int type = listType(l);
+  int type = listType(&l);
 
+  cout << type << '\n';
   // each type has different necessities.
   switch (type)
   {
   case 1:
-    type1Sort(l);
+    type1Sort(&l);
     break;
   case 2:
-    type2Sort(l);
+    type2Sort(&l);
     break;
   case 3:
-    type3Sort(l);
+    type3Sort(&l);
     break;
   default:
     // if we reach default, we just use bad sort.
